7bd08d2784c65cdab88ffd4f899d4c6e7f8a390d
Mahek <maheksolanki005@gmail.com>
2025-07-06 10:01:42 +0530

    latest commit

--- server/server.js
+++ server/server.js
@@ -1,27 +1,35 @@
-import express from 'express';
-import cors from 'cors';
-import 'dotenv/config';
-import connectDB from './configs/db.js';
-import { clerkMiddleware } from '@clerk/express'
+import express from "express";
+import cors from "cors";
+import "dotenv/config";
+import connectDB from "./configs/db.js";
+import { clerkMiddleware } from "@clerk/express";
 import { serve } from "inngest/express";
-import { inngest, functions } from "./inngest/index.js"
+import { inngest, functions } from "./inngest/index.js";
 
 const app = express();
 const port = 3000;
 
-await connectDB(); // this is for connecting db from db.js file 
+await connectDB(); // this is for connecting db from db.js file
 
 //middleware
 app.use(express.json()); // use to parse the json data to req.body
 app.use(cors()); //	Allows frontend to talk to backend (CORS)
-app.use(clerkMiddleware())
+app.use(clerkMiddleware());
 
 //API Routes
-app.get('/',(req,res)=>{
+app.get("/", (req, res) => {
   res.send("server is Live!");
-})
-app.use('/api/inngest',serve({ client: inngest, functions ,signingKey: process.env.INGGEST_SIGNING_KEY, })) // api endpoin for inngest
+});
+app.use(
+  "/api/inngest",
+  serve({
+    client: inngest,
+    signingKey: process.env.INNGEST_SIGNING_KEY,
+    functions,
+    resync: true,
+  })
+); // api endpoin for inngest
 
-app.listen(port , ()=>{
+app.listen(port, () => {
   console.log(`server running on port ${port}`);
-})
+});
